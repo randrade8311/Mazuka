c3d9d8e325dca12275c6108e2ff4e5daaaeba36b
Rodrigo Andrade <randrade709@gmail.com>
2025-02-27 10:15:29 -0800

    Added mobile animations for gallery section

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     if(window.innerWidth <= 768) {
         const elements = document.querySelectorAll(".scrollBlock");
-        
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     entry.target.classList.add("animate");
                 }
             });
-        }, {threshold: 0.3});
+        }, {threshold: 0.1});
     
         elements.forEach(el => observer.observe(el));
     }
